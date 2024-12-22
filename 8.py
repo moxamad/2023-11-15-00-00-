d203af4b8f727f5d45fcd406f905ffb3e1f808d2
moxamad <R8AGn5VhFSy7Rz@yandex.ru>
2024-12-22 18:35:42 +0300

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,47 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+
+class Shop:
+    __file_name = 'products.txt'
+    file = open(__file_name, 'a')
+    temp = file.write('')
+    file.close()
+
+    def get_products(self):
+        file = open(self.__file_name,'r')
+        temp = str(file.read())
+        file.close()
+        return temp
+
+    def add(self, *products):
+        for i in products:
+            if str(i) in self.get_products():
+                print(f'Продукт {i} уже есть в магазине')
+
+            else:
+                file = open(self.__file_name, 'a')
+                file.write(str(i))
+
+                file.write('\n')
+                file.close()
+
+
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
